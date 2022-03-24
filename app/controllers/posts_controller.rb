f7322a29a56b22ee092a1ab0f310e6d23621ee5b
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-24 23:42:15 +0100

    modal show update/destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,24 @@ class PostsController < ApplicationController
     @comments = Comment.where(post: @post)
     @comment = Comment.new
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :posts, only: :index
+  resources :posts, only: [:index, :create, :update, :destroy]
   resources :posts, only: :show do
     resources :comments, only: [:index, :create]
   end
