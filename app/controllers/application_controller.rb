5f69709486e3766e8dd13e1d83b80784666938cd
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-26 18:04:03 +0100

    host

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
+    { host: "certif-challenge-simon.herokuapp.com" }
   end
 
   protected
